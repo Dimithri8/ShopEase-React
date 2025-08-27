50bd88fe495566571d6e735f44ee475430c17fc9
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-27 22:53:09 +0530

    Built and styled the UI for header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,48 @@
+// Icon imports
+import LocalShippingOutlinedIcon from "@mui/icons-material/LocalShippingOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import LocalMallOutlinedIcon from "@mui/icons-material/LocalMallOutlined";
+import PermIdentityOutlinedIcon from "@mui/icons-material/PermIdentityOutlined";
+import { Link } from "react-router-dom";
+
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header id="header">
+      <div className="logoContainer">
+        <span className="logo">ShopEase</span>
+      </div>
+      <form className="searchContainer">
+        <input
+          className="searchBar"
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search for products"
+        />
+        <button className="primary-btn" type="button">
+          Search
+        </button>
+      </form>
+      <nav className="accountContainer">
+        <Link to={"/orders"} aria-label="Orders" className="nav-link">
+          <span className="link-name">Orders</span>
+          <LocalShippingOutlinedIcon />
+        </Link>
+        <Link to={"/orders"} aria-label="Saved" className="nav-link">
+          <span className="link-name">Saved</span>
+          <FavoriteBorderOutlinedIcon />
+        </Link>
+        <Link to={"/orders"} aria-label="My Cart" className="nav-link">
+          <span className="link-name">My Cart</span>
+          <LocalMallOutlinedIcon />
+        </Link>
+        <Link to={"/orders"} aria-label="Profile" className="nav-link">
+          <span className="link-name">Profile</span>
+          <PermIdentityOutlinedIcon />
+        </Link>
+      </nav>
+    </header>
+  );
+}

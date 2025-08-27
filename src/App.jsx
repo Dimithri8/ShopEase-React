839337a14ef5a7c30e6f11a5bc654f63f4f24b93
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-27 21:05:12 +0530

    Added Browser Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+import Header from "./components/Header";
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+    </BrowserRouter>
   );
 }
 

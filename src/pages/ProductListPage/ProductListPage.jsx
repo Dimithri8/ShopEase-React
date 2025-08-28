2ab63f680bbd22916d68b5f1eb1c7d9a8ce4950c
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-28 15:48:23 +0530

    Add logic to fetch  data from local json file and render product cards

--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -1,18 +1,35 @@
+import { useState, useEffect } from "react";
+
 import Header from "../../components/Header/Header";
 import Filter from "../../components/Filters/Filter";
 import ProductCard from "../../components/ProductCard/ProductCard";
 
 import "./ProductListPage.css";
+
 export default function ProductListPage() {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
+    try {
+      const response = await fetch(`http://localhost:3001/products`);
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(`Error couldn't fetch data`, error);
+    }
+  }
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <>
       <Header />
       <Filter />
       <section className="product-cards-container">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </section>
     </>
   );

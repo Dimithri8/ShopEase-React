3f3c172dfbb0e832f26ad5af582aaddd5a689693
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-28 15:00:15 +0530

    Added Product Card to Product listing page

--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -1,11 +1,19 @@
 import Header from "../../components/Header/Header";
 import Filter from "../../components/Filters/Filter";
+import ProductCard from "../../components/ProductCard/ProductCard";
 
+import "./ProductListPage.css";
 export default function ProductListPage() {
   return (
     <>
       <Header />
       <Filter />
+      <section className="product-cards-container">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </section>
     </>
   );
 }

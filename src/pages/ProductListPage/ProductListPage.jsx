bd12f233efcaf32abf8374a3c0e5e9032e54dc24
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-28 14:11:47 +0530

    Created a seperate page for listing products

---
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header/Header";
+import Filter from "../../components/Filters/Filter";
+
+export default function ProductListPage() {
+  return (
+    <>
+      <Header />
+      <Filter />
+    </>
+  );
+}

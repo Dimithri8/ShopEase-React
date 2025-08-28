c9c63f9f6772abb50cbdcf99ab33c81e6982b53b
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-28 20:30:14 +0530

    Added search logic

--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -7,12 +7,14 @@ import ProductCard from "../../components/ProductCard/ProductCard";
 import "./ProductListPage.css";
 
 export default function ProductListPage() {
+  const [allProducts, setAllProducts] = useState([]);
   const [products, setProducts] = useState([]);
-
+  const [search, setSearch] = useState("");
   async function getProducts() {
     try {
       const response = await fetch(`http://localhost:3001/products`);
       const data = await response.json();
+      setAllProducts(data);
       setProducts(data);
     } catch (error) {
       console.log(`Error couldn't fetch data`, error);
@@ -22,9 +24,26 @@ export default function ProductListPage() {
     getProducts();
   }, []);
 
+  function handleChange(event) {
+    let userInput = event.target.value;
+    setSearch(userInput);
+    console.log(`Search term: ${search}`);
+  }
+
+  function handleClick() {
+    if (!search) {
+      setProducts(allProducts);
+    } else {
+      setProducts(allProducts.filter((product) => product.name === search));
+    }
+  }
   return (
     <>
-      <Header />
+      <Header
+        input={search}
+        handleChange={handleChange}
+        handleClick={handleClick}
+      />
       <Filter />
       <section className="product-cards-container">
         {products.map((product) => (

2b2e1cecd903ab53632d3a5ddb5b2b81394d0d4c
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-29 15:33:48 +0530

    Added product card click functionality

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,21 @@
-import { Card, CardMedia, CardContent, Typography } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActionArea,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 import "./ProductCard.css";
 
 export default function ProductCard({ product }) {
+  const navigate = useNavigate();
+
+  function handleCardClick() {
+    navigate(`/details/${product.id}/${product.name}`);
+  }
   return (
     <>
       <section className="productCardsContainer">
@@ -20,39 +32,41 @@ export default function ProductCard({ product }) {
             },
           }}
         >
-          <CardMedia
-            component={"img"}
-            sx={{
-              height: 250,
-              borderRadius: 1,
-              objectFit: "cover",
-              objectPosition: "center",
-            }}
-            src={product.image}
-          />
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 16, fontWeight: 400 }}
-              variant="h3"
-              component={"div"}
-            >
-              {product.name}
-            </Typography>
-
-            <div className="card-actions-wrapper">
-              <Typography variant="p" sx={{ fontWeight: 700 }}>
-                {product.price}
+          <CardActionArea onClick={handleCardClick}>
+            <CardMedia
+              component={"img"}
+              sx={{
+                height: 250,
+                borderRadius: 1,
+                objectFit: "cover",
+                objectPosition: "center",
+              }}
+              src={product.image}
+            />
+            <CardContent>
+              <Typography
+                sx={{ fontSize: 16, fontWeight: 400 }}
+                variant="h3"
+                component={"div"}
+              >
+                {product.name}
               </Typography>
 
-              <FavoriteBorderIcon
-                sx={{
-                  "&:hover": {
-                    cursor: "pointer",
-                  },
-                }}
-              />
-            </div>
-          </CardContent>
+              <div className="card-actions-wrapper">
+                <Typography variant="p" sx={{ fontWeight: 700 }}>
+                  {product.price}
+                </Typography>
+
+                <FavoriteBorderIcon
+                  sx={{
+                    "&:hover": {
+                      cursor: "pointer",
+                    },
+                  }}
+                />
+              </div>
+            </CardContent>
+          </CardActionArea>
         </Card>
       </section>
     </>

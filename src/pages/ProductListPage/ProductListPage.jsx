58f71f79ba89db4e2b7a6dc916a7d070f471d181
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-29 12:28:33 +0530

    Added product filtering logic

--- src/pages/ProductListPage/ProductListPage.jsx
+++ src/pages/ProductListPage/ProductListPage.jsx
@@ -10,6 +10,9 @@ export default function ProductListPage() {
   const [allProducts, setAllProducts] = useState([]);
   const [products, setProducts] = useState([]);
   const [search, setSearch] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedCondition, setSelectedCondition] = useState("");
+  const [selectedPrice, setSelectedPrice] = useState("");
   async function getProducts() {
     try {
       const response = await fetch(`http://localhost:3001/products`);
@@ -24,18 +27,52 @@ export default function ProductListPage() {
     getProducts();
   }, []);
 
+  useEffect(() => {
+    applyFilters();
+  }, [selectedCategory, selectedCondition, selectedPrice]);
+
   function handleChange(event) {
     let userInput = event.target.value;
     setSearch(userInput);
     console.log(`Search term: ${search}`);
   }
-
+  function clearFilter() {
+    setProducts(allProducts);
+    setSelectedCategory("");
+    setSelectedCondition("");
+    setSelectedPrice("");
+  }
   function handleClick() {
     if (!search) {
       setProducts(allProducts);
     } else {
       setProducts(allProducts.filter((product) => product.name === search));
     }
+  }
+  function applyFilters() {
+    let filtered = allProducts
+      .filter(
+        (product) => !selectedCategory || product.category === selectedCategory
+      )
+      .filter(
+        (product) =>
+          !selectedCondition || product.condition === selectedCondition
+      )
+      .filter((product) => !selectedPrice || product.price <= selectedPrice);
+    setProducts(filtered);
+  }
+  function selectCategory(event) {
+    let selectedValue = event.target.value;
+    setSelectedCategory(selectedValue);
+  }
+
+  function selectConditon(event) {
+    let selectedValue = event.target.value;
+    setSelectedCondition(selectedValue);
+  }
+  function selectPrice(event) {
+    let selectedValue = event.target.value;
+    setSelectedPrice(selectedValue);
   }
   return (
     <>
@@ -44,7 +81,19 @@ export default function ProductListPage() {
         handleChange={handleChange}
         handleClick={handleClick}
       />
-      <Filter />
+
+      <Filter
+        category={allProducts.map((product) => product.category)}
+        condition={allProducts.map((product) => product.condition)}
+        selectCategory={selectCategory}
+        selectConditon={selectConditon}
+        clearFilter={clearFilter}
+        selectPrice={selectPrice}
+        selectedCategory={selectedCategory}
+        selectedCondition={selectedCondition}
+        selectedPrice={selectedPrice}
+      />
+
       <section className="product-cards-container">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />

582ff5319ca50c53767dde0cc089107c733f1812
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-29 15:36:33 +0530

    Setup routes for page details

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ProductListPage from "./pages/ProductListPage/ProductListPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage/ProductDetailsPage";
 function App() {
   return (
     <BrowserRouter>
-      <ProductListPage />
+      <Routes>
+        <Route path="/" element={<ProductListPage />} />
+        <Route path="/details/:id/:name" element={<ProductDetailsPage />} />
+      </Routes>
     </BrowserRouter>
   );
 }

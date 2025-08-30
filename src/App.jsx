619b6e91ef1c003d2f002917448d15c62c30619d
Dimithri8 <dimithridananjaya924@gmail.com>
2025-08-30 16:32:35 +0530

    Added route path for cart page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import ProductListPage from "./pages/ProductListPage/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage/ProductDetailsPage";
+import Cart from "./pages/Cart/Cart";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<ProductListPage />} />
         <Route path="/details/:id/:name" element={<ProductDetailsPage />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
   );
